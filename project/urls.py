4809db8680ddd43028871d9542ffaa3fb1349932
Francesco Salvestrini <salvestrini@gmail.com>
2011-05-04 01:02:57 +0200

    Updated, added main app, added 404 and 505 templates

--- project/urls.py
+++ project/urls.py
@@ -1,16 +1,24 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults   import *
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^site/', include('site.foo.urls')),
+urlpatterns = \
+    patterns('',
+             (r'^admin/doc/',
+              include('django.contrib.admindocs.urls')),
+             
+             (r'^admin/',
+              include('admin.site.urls')),
+             
+             ('^about/$',
+              direct_to_template, { 'template' : 'about.html' }),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+             ('^$',
+              direct_to_template, { 'template' : 'index.html' }),
 
-    # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
-)
+             #('^.*$',
+             # handler404),
+             )
