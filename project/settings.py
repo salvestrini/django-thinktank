458fa453cee04631da3bb36779437cb3cfd61632
Francesco Salvestrini <salvestrini@gmail.com>
2011-08-01 18:55:37 +0200

    Fixed settings

--- project/settings.py
+++ project/settings.py
@@ -102,3 +102,21 @@ INSTALLED_APPS = (
     'main',
     'photo',
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
